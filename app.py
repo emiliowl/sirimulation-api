4719e773576bd28f8e38be8d0a68c75e8f9fcbb2
Emilio Murta <emilio.murta@msitbrasil.com>
2019-07-04 22:28:50 -0300

    sirimulation

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask
+from fuzzywuzzy import process
+from datetime import datetime
+
+app = Flask(__name__)
+database = ["pêra", "uva", "maçã", "salada mista"]
+
+@app.route('/simulation/<string:say_my_name>', methods=['GET', 'POST'])
+def hello_world(say_my_name):
+    ret = f'{datetime.now()} // Calculating similarities <br />'
+    process.extract(say_my_name, database)
+    selection = process.extractBests(query=say_my_name, choices=database, limit=5)
+    for el in selection:
+        ret = f"{ret}{datetime.now()} //{el[0]} x {say_my_name}: {el[1]}<br />"
+    return ret
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='localhost', port=port)
