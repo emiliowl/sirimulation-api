b190156daf58152497171ec8c6c0d1c83b086685
emilio murta resende <eresende@thinkbrq.com>
2019-07-06 14:24:02 -0300

    mongo cloud

--- app.py
+++ app.py
@@ -1,28 +1,33 @@
 from nosql import get_data
 
-import os
-from flask import Flask
+import os, ssl
+from flask import Flask, jsonify
 from fuzzywuzzy import process
 from datetime import datetime
 
+if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
+    getattr(ssl, '_create_unverified_context', None)):
+    ssl._create_default_https_context = ssl._create_unverified_context
+
 app = Flask(__name__)
+database = get_data()
 
 @app.route('/simulation/<string:say_my_name>', methods=['GET', 'POST'])
 def hello_world(say_my_name):
-    ret = f'{datetime.now()} // Calculating similarities <br />'
-    process.extract(say_my_name, database)
-    selection = process.extractBests(query=say_my_name, choices=database, limit=5)
-    for el in selection:
-        ret = f"{ret}{datetime.now()} //{el[0]} x {say_my_name}: {el[1]}<br />"
-    return ret
+    print(f'{datetime.now()} // Calculating similarities ...')
+    selection = process.extractBests(query=say_my_name, choices=[el["TRIM"] for el in database], limit=5)
+    print(f"{datetime.now()} // Process finished, returning data ...")
 
-def initialize_database():
-    text_file = open("nosql.txt", "r")
-    lines = text_file.readlines()
-    text_file.close()
-    return lines
+    return_data = []
+    for sel in selection:
+        next_sel = next(vehicle for vehicle in database if vehicle["TRIM"] == sel[0])
+        if next_sel != None:
+            next_sel['ACCURACY'] = sel[1]
+        if next_sel != None and '_id' in next_sel:
+            del next_sel['_id']
+        return_data.append(next_sel)
 
-database = get_data()
+    return jsonify(return_data)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
--- load_database.py
+++ load_database.py
@@ -1,13 +1,13 @@
-import os
+import json
 from pymongo import MongoClient
-#client = MongoClient("localhost", 27017)
-cert_path = os.path.dirname(os.path.realpath(__file__))
+import ssl
+
 client = MongoClient("mongodb://admin:fDwT14qEMTvc2V9f@SG-Sirimulation-22916.servers.mongodirector.com:49076,SG-Sirimulation-22917.servers.mongodirector.com:49076,SG-Sirimulation-22918.servers.mongodirector.com:49076/admin?replicaSet=RS-Sirimulation-0&ssl=true",
                      ssl=True,
-                     ssl_ca_certs=f'{cert_path}{os.path.sep}cert.pem')
+                     ssl_ca_certs='cert.pem')
 vehicles_conn = client.vehicles
 
-raw_data = vehicles_conn.vehicles.find()
-
-def get_data():
-    return list(raw_data)
+with open('vehicles.json', 'r') as file:
+    data = file.read().replace('\n', '')
+    registers = list(json.loads(data)["Vehicles"])
+    vehicles_conn.vehicles.insert_many(registers)
